9bf09d261138480292dfad26753c5f90dae621a0
raeadvincula <raejillian.advincula@benilde.edu.ph>
2019-01-08 16:57:09 +0800

    added a comment

--- app/src/main/java/com/example/raeadvincula/rhea/MainPage.java
+++ app/src/main/java/com/example/raeadvincula/rhea/MainPage.java
@@ -9,5 +9,8 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        //pogi si gerald
+        // rsefgh
     }
 }
