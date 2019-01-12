e9ee4bc8654118ca2f15f716660c683463030f98
raeadvincula <raejillian.advincula@benilde.edu.ph>
2019-01-13 00:31:51 +0800

    for testing

--- app/src/main/java/com/example/raeadvincula/rhea/MainDiary.java
+++ app/src/main/java/com/example/raeadvincula/rhea/MainDiary.java
@@ -3,15 +3,13 @@ package com.example.raeadvincula.rhea;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainPage extends AppCompatActivity {
+public class MainDiary extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
-
-        //pogi si gerald
-        // rsefgh
-        // gfjnk
+        setContentView(R.layout.activity_main_diary);
     }
+
+    // tests
 }
